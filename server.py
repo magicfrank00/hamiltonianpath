5ab488684c1ac01b908a3090a381b55db63a96c0
vmcvastry <009valerio@gmail.com>
2024-09-04 00:19:07 +0200

    prover tester

--- server.py
+++ server.py
@@ -1,75 +1,86 @@
-from utils import pedersen_commit, pedersen_open
-from utils import commit_to_graph, open_graph, permute_graph
-from utils import hash_committed_graph, test_cycle, check_graph
-from utils import public_parameters
-from utils import check_permutation
-from utils import numrounds
 import json
-import os
+from utils import (
+    open_graph,
+    permute_graph,
+    hash_committed_graph,
+    test_cycle,
+    check_graph,
+    check_permutation,
+    numrounds,
+)
 
 
-N = 5
-G = [
-    [0,1,1,0,1],
-    [1,0,0,0,0],
-    [0,0,0,1,0],
-    [0,1,1,0,0],
-    [1,0,1,1,0]
-]
+class HamiltonianCycleTester:
+    def __init__(self, N, G):
+        self.N = N
+        self.G = G
+        self.FS_state = b""
+        self.A_vals = []
+        self.z_vals = []
 
-print(f'prove to me that G has a hamiltonian cycle!')
+    def prove_hamiltonian_cycle(self):
+        print(f"prove to me that G has a hamiltonian cycle!")
+        for i in range(numrounds):
+            payload = json.loads(input(b"send fiat shamir proof: "))
+            A = payload["A"]
+            z = payload["z"]
 
-# 128 bit security
-FS_state = b''
+            check_graph(A, self.N)
 
-A_vals = []
-z_vals = []
-for i in range(numrounds):
-    # send permuted commitment matrix
-    payload = json.loads(input(b"send fiat shamir proof: "))
-    A = payload["A"]
-    z = payload["z"]
+            self.A_vals.append(A)
+            self.z_vals.append(z)
 
-    check_graph(A,N)
+        self.compute_fiat_shamir_challenge()
 
-    A_vals.append(A)
-    z_vals.append(z)    
+        for i in range(numrounds):
+            print(f"checking round {i}")
+            challenge = int(self.challenge_bits[i])
+            print(f"challenge bit is {challenge}")
+            A = self.A_vals[i]
+            z = self.z_vals[i]
 
-print("computing fiat shamir challenge")
-for i in range(numrounds):
-    FS_state = hash_committed_graph(A_vals[i], FS_state)
+            if challenge:
+                self.verify_cycle(A, z)
+            else:
+                self.verify_permutation(A, z)
 
-challenge_bits = bin(int.from_bytes(FS_state, 'big'))[-numrounds:]
+        print("you've convinced me it has a hamiltonian path! Cool!")
 
-for i in range(numrounds):
-    print(f"checking round {i}")
-    challenge = int(challenge_bits[i])
-    print(f"challenge bit is {challenge}")
-    A = A_vals[i]
-    z = z_vals[i]
-    # Challenge bit is 1:
-    # You should open the hamiltonian path
-    # z = [cycle, openings of cycle]
-    if challenge:
+    def compute_fiat_shamir_challenge(self):
+        print("computing fiat shamir challenge")
+        for i in range(numrounds):
+            self.FS_state = hash_committed_graph(self.A_vals[i], self.FS_state)
+
+        self.challenge_bits = bin(int.from_bytes(self.FS_state, "big"))[-numrounds:]
+
+    def verify_cycle(self, A, z):
         cycle, openings = z
-        if not test_cycle(A, N, cycle, openings):
+        if not test_cycle(A, self.N, cycle, openings):
             print("your proof didn't verify :(")
             exit()
         else:
             print("accepted")
-    
-    # challenge bit is 0:
-    # you should show permutation and open everything
-    # z = [permutation, openings of everything]
-    else:
+
+    def verify_permutation(self, A, z):
         permutation, openings = z
-        check_permutation(permutation, N)
-        G_permuted = open_graph(A,N, openings)
-        G_test = permute_graph(G, N, permutation)
+        check_permutation(permutation, self.N)
+        G_permuted = open_graph(A, self.N, openings)
+        G_test = permute_graph(self.G, self.N, permutation)
         if G_permuted == G_test:
             print("accepted")
         else:
-            print("your proof didn't verify :()")
+            print("your proof didn't verify :(")
             exit()
-    
-print("you've convinced me it has a hamiltonian path! Cool!")
+
+
+if __name__ == "__main__":
+    N = 5
+    G = [
+        [0, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0],
+        [0, 1, 1, 0, 0],
+        [1, 0, 1, 1, 0],
+    ]
+    proof = HamiltonianCycleProof(N, G)
+    proof.prove_hamiltonian_cycle()
